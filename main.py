b5d3c12af46c3acbc262842d6973a5526be176c7
David Corcoran <44246360+ArcaneIRE@users.noreply.github.com>
2024-10-05 19:26:22 +0100

    Clean up main.py

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
-import json
-import astropy.units as u
-from astropy.coordinates import SkyCoord
 from astroquery.gaia import Gaia
 from astroquery.ipac.nexsci.nasa_exoplanet_archive import NasaExoplanetArchive as nea
-import convert
 
 Gaia.ROW_LIMIT = 1000
-coord = SkyCoord(ra=280, dec=-60, unit=(u.degree, u.degree), frame='icrs')
-# j = Gaia.cone_search_async(coord, radius=u.Quantity(1.0, u.deg),dump_to_file=True, output_format='csv')
 
 query = """
 SELECT 
@@ -24,23 +18,15 @@ WHERE
     AND dec BETWEEN -47.7883 AND -43.7883
 """
 
-j = Gaia.launch_job(query, dump_to_file=True, output_format='csv')
-r = j.get_results()
+job = Gaia.launch_job(query, dump_to_file=True, output_format='csv')
+stars = job.get_results()
 
 pl_table = "ps"
 pl_select = "DISTINCT pl_name, ra, dec, sy_dist"
 pl_where = "pl_name = 'Kepler-22 b'"
 
-j_pl = nea.query_criteria(table=pl_table, select=pl_select, where=pl_where)
+planets = nea.query_criteria(table=pl_table, select=pl_select, where=pl_where)
 
-## add cartiesian conv
-
-print(r)
-print(j_pl)
-
-print(type(r))
-print(type(j_pl))
-
-r.write("a.csv", overwrite=True)
-j_pl.write("b.csv", overwrite=True)
-
+# Write
+stars.write("stars.csv", overwrite=True)
+planets.write("planets.csv", overwrite=True)
