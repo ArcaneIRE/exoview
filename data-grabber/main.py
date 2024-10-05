5b5ea3e02caff494d9e142497be64a47f586c6a6
Ugo <73886820+hollop90@users.noreply.github.com>
2024-10-05 21:45:42 +0000

    make code fro csv serialization

--- data-grabber/main.py
+++ data-grabber/main.py
@@ -22,3 +22,11 @@ with open("planet.csv", "w") as f:
     tb = astropy.table.Table(planets)
     d_writer.writerow(tb[0])
 
+# make star CSV for planet
+r = get_stars(planets[0])
+with open("star.csv", "w") as f:
+    fieldnames = r.colnames
+    d_writer = csv.DictWriter(f, fieldnames=fieldnames)
+    d_writer.writeheader()
+    tb = astropy.table.Table(r)
+    d_writer.writerows(tb)
