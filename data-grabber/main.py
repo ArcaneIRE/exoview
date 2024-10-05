e9b949eb48fbb0ffdf480445decaf9fc2eccc9e2
David Corcoran <44246360+ArcaneIRE@users.noreply.github.com>
2024-10-05 23:22:59 +0100

    Layout main.py

--- data-grabber/main.py
+++ data-grabber/main.py
@@ -8,42 +8,16 @@ from astroquery.gaia import Gaia
 from astroquery.ipac.nexsci.nasa_exoplanet_archive import NasaExoplanetArchive as nea
 from get_random_exoplanets import *
 from get_rand_stars import *
-import csv
 import astropy.table
+from write_tables
 
 # Get a bunch of planets
 planets = get_random_exoplanets(5)
-points_of_view = []
-# Pair up each planet with a table of stars
 for planet in planets:
-    pov = {
-        'planet': planet,
-        'stars': get_stars(planet),
-    }
-
-for pov in points_of_view:
-    # add cartesians relative to earth
-    # add translated cartesians with XO as origin
-    # get galactic coords with XO as origin
-    # calculate "real brightness"
-    # remove stars with a brightness above 6.5
-    pass
-
-# Write Tables to CSV
-for pov in points_of_view:
-    # Make planet CSV
-    with open("planet.csv", "w") as f:
-        fieldnames = planets.colnames
-        d_writer = csv.DictWriter(f, fieldnames=fieldnames)
-        d_writer.writeheader()
-        tb = astropy.table.Table(planets)
-        d_writer.writerow(tb[0])
-
-    # Make stars CSV for planet
-    r = get_stars(planets[0])
-    with open("star.csv", "w") as f:
-        fieldnames = r.colnames
-        d_writer = csv.DictWriter(f, fieldnames=fieldnames)
-        d_writer.writeheader()
-        tb = astropy.table.Table(r)
-        d_writer.writerows(tb)
+    stars = get_stars(planet)
+    # add_cartesians(planet, stars)
+    # add_XO_origin_cartesians(planet, stars)
+    # add_XO_origin_galactics(planet, stars)
+    # calculate_real_brightness(stars)
+    # cull_dim_stars(stars)
+    write_tables(planet, stars)
