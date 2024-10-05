260f941cca753cb88c67a4e2211c246b46c39a8f
David Corcoran <44246360+ArcaneIRE@users.noreply.github.com>
2024-10-05 19:29:48 +0100

    Tidy tables.py

--- tables.py
+++ tables.py
@@ -1,26 +1,18 @@
 import csv
 import convert
 
-# Open the CSV file
-with open('a.csv', 'r') as file:
-    # Create a CSV reader object
+with open('stars.csv', 'r') as file:
     reader = csv.DictReader(file)
 
-    # Open a new file for writing
-    with open('a_new.csv', 'w', newline='') as output_file:
-        # Create a CSV writer object
+    with open('stars_with_cartesian.csv', 'w', newline='') as output_file:
         fieldnames = reader.fieldnames + ['cart_x', 'cart_y', 'cart_z']
         writer = csv.DictWriter(output_file, fieldnames=fieldnames)
         writer.writeheader()
 
-        # Iterate over the rows in the CSV file
         for row in reader:
-
-            # Write the row to the new CSV file
-            x, y, z = convert.galacticToCartesian(row['l'], row['b'], row['distance_gspphot'])
+            x, y, z = convert.galacticToCartesian(
+                row['galactic_longitude'], row['galactic_latitude'], row['distance_gspphot'])
             row['cart_x'] = x
             row['cart_y'] = y
             row['cart_z'] = z
-
             writer.writerow(row)
-
