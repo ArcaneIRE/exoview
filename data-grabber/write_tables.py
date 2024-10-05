5d2720071780bc95a3040cd8c9aa67f1c777799e
David Corcoran <44246360+ArcaneIRE@users.noreply.github.com>
2024-10-05 23:13:13 +0100

    Add write_tables.py

---
+++ data-grabber/write_tables.py
@@ -0,0 +1,18 @@
+def write_tables(planets, stars):
+    pass
+    # Make planet CSV
+    # with open("planet.csv", "w") as f:
+    #     fieldnames = planets.colnames
+    #     d_writer = csv.DictWriter(f, fieldnames=fieldnames)
+    #     d_writer.writeheader()
+    #     tb = astropy.table.Table(planets)
+    #     d_writer.writerow(tb[0])
+
+    # # Make stars CSV for planet
+    # r = get_stars(planets[0])
+    # with open("star.csv", "w") as f:
+    #     fieldnames = r.colnames
+    #     d_writer = csv.DictWriter(f, fieldnames=fieldnames)
+    #     d_writer.writeheader()
+    #     tb = astropy.table.Table(r)
+    #     d_writer.writerows(tb)
