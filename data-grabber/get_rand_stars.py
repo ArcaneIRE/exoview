c0f8e93e9312549f842a67e46e89615a2047d89f
Ugo <nozolo96@gmail.com>
2024-10-06 09:58:22 +0100

    add magnitude

--- data-grabber/get_rand_stars.py
+++ data-grabber/get_rand_stars.py
@@ -13,16 +13,17 @@ def get_stars(planet):
     pc = float(planet['sy_dist'].value)
 
     query = f"""
-    SELECT 
+    SELECT
         distance_gspphot,
         ra,
         dec,
         l,
         b,
-        pseudocolour
-    FROM 
+        pseudocolour,
+        phot_g_mean_mag
+    FROM
         "gaiadr3"."gaia_source"
-    WHERE 
+    WHERE
         distance_gspphot BETWEEN {pc-40} AND {pc+40}
         AND ra BETWEEN {ra-2} AND {ra+2}
         AND dec BETWEEN {dec-2} AND {dec+2}
